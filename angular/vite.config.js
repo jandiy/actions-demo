d0b89bfbb33bd6d58bba426e0007682f6a8021f2
jandira <vargas111997@gmail.com>
2026-05-13 14:38:35 -0400

    add files unfinded

--- angular/vite.config.js
+++ angular/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
+    reporters: ['json'],
+    outputFile: 'test.json'
   },
 });
